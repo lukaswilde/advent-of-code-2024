3130da120489c8ab026cef02d6c987131e28cb2c
Lukas Wilde <s8luwild@stud.uni-saarland.de>
2024-12-08 16:02:26 +0100

    add utility function to split input texts seperated by two newlines

--- utils/input.py
+++ utils/input.py
@@ -1,13 +1,28 @@
 import inspect
+from typing import List
 from pathlib import Path
 
 
 def read_input(file_name: str) -> str:
     """Returns the text content of an input file given relative to a dayXX folder"""
-
-    # Extract the location of the calling function, to correctly get the absolute puzzle input path
-    calling_module = inspect.stack()[-1].filename
-    file = Path(calling_module).resolve().parent / file_name
+    file = _correct_calling_directory(file_name)
 
     with file.open('r') as f:
         return f.read()
+
+
+def _correct_calling_directory(file_name:str) -> Path:
+    # Extract the location of the calling function, to correctly get the absolute puzzle input path
+    calling_module = inspect.stack()[-1].filename
+    return Path(calling_module).resolve().parent / file_name
+
+def read_split_input(file_name: str) -> List[str]:
+    """
+    Return the text content of an input file given relative to a dayXX folder.
+    The input is expected to be split by two newlines
+    """
+    file = _correct_calling_directory(file_name)
+
+    with file.open("r") as f:
+        return f.read().split("\n\n")
+
