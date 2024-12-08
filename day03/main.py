35cec8706742539e45d1e99b49b80217c375dab4
Lukas Wilde <s8luwild@stud.uni-saarland.de>
2024-12-08 02:08:29 +0100

    add part2 of day03

--- day03/main.py
+++ day03/main.py
@@ -1,40 +1,51 @@
-from collections import Counter
-from typing import List
 import re
+from typing import List
 
 from input import read_input
 
 
 def extract_muls(file_name: str) -> List[str]:
     input_text = read_input(file_name)
-    pattern = re.compile(r"mul\(\d{1,3},\d{1,3}\)")
+    pattern = re.compile(r'mul\(\d{1,3},\d{1,3}\)')
     return re.findall(pattern, input_text)
 
 
-def calculate_uncorrupted_sum(muls: List[str]) -> int:
-    stripped = [mul.replace("mul", "").replace("(", "").replace(")", "").split(',') for mul in muls]
+def extract_muls_conditionals(file_name: str) -> List[str]:
+    input_text = read_input(file_name)
+    pattern = re.compile(r"(mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\))")
+    return re.findall(pattern, input_text)
+
+
+def calculate_uncorrupted_sum(instructions: List[str]) -> int:
+    assert "don't()" not in instructions
+    stripped = [
+        instruction.replace('mul', '').replace('(', '').replace(')', '').split(',')
+        for instruction in instructions
+        if instruction != 'do()'
+    ]
+    assert 'do' not in stripped
     return sum(int(x[0]) * int(x[1]) for x in stripped)
 
 
-# def calculate_similarity(list1: List[int], list2: List[int]) -> int:
-#     occurrences = Counter(list2)
-#     return sum(map(lambda x: x * occurrences[x], list1))
+def calculate_uncorrupted_conditional_sum(muls: List[str]) -> int:
+    DELIMITER = 'X'
+    filtered = re.sub(rf"don't\(\){DELIMITER}.*?do\(\){DELIMITER}", '', DELIMITER.join(muls))
+    return calculate_uncorrupted_sum(filtered.split(DELIMITER))
 
 
 def part1(file_name: str) -> int:
     muls = extract_muls(file_name)
     return calculate_uncorrupted_sum(muls)
-    # return calculate_differences(*lists)
 
 
-# def part2(file_name: str) -> int:
-#     lists = extract_muls(file_name)
-#     return calculate_similarity(*lists)
-#
+def part2(file_name: str) -> int:
+    instructions = extract_muls_conditionals(file_name)
+    return calculate_uncorrupted_conditional_sum(instructions)
 
-if __name__ == "__main__":
-    result1 = part1("puzzle.txt")
-    print(f"Total sum of uncorrupted multiplications: {result1}")
 
-    # result2 = part2("puzzle.txt")
-    # print(f"Similarity score of the two lists: {result2}")
+if __name__ == '__main__':
+    result1 = part1('puzzle.txt')
+    print(f'Total sum of uncorrupted multiplications: {result1}')
+
+    result2 = part2('puzzle.txt')
+    print(f'Total sum of uncorrupted multiplications and conditionals: {result2}')
