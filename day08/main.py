98ee164b2c49b747a0d1b0ebda36cc3401352d47
Lukas Wilde <s8luwild@stud.uni-saarland.de>
2024-12-10 01:38:45 +0100

    add day08

---
+++ day08/main.py
@@ -0,0 +1,87 @@
+from collections import defaultdict
+from typing import List, Tuple
+
+from geometry import Grid, Vec2d
+from input import read_input
+
+
+class Map(Grid):
+    def __init__(self, repr: str):
+        super().__init__(repr)
+
+        self.antennas = defaultdict(list)
+
+        for i, line in enumerate(self.lines):
+            for j, char in enumerate(line):
+                if char == '.':
+                    continue
+                self.antennas[char].append(Vec2d(i, j))
+
+        self.combinations = self._calculate_combinations()
+
+    def _calculate_combinations(self) -> List[Tuple[Vec2d, Vec2d]]:
+        res = []
+        for same_type in self.antennas.values():
+            for i, pos1 in enumerate(same_type):
+                for pos2 in same_type[i + 1 :]:
+                    res.append((pos1, pos2))
+
+        return res
+
+
+def calculate_unique_antinodes(map: Map) -> int:
+    unique_antinodes = set()
+
+    for pos1, pos2 in map.combinations:
+        difference = pos1 - pos2
+        if not map.out_of_bounds(antinode := pos1 + difference):
+            unique_antinodes.add(antinode)
+        if not map.out_of_bounds(antinode := pos2 - difference):
+            unique_antinodes.add(antinode)
+
+    return len(unique_antinodes)
+
+
+def calculate_resonant_antinodes(map: Map) -> int:
+    resonant_antinodes = set()
+
+    for pos1, pos2 in map.combinations:
+        resonant_antinodes.add(pos1)
+        resonant_antinodes.add(pos2)
+
+        difference = pos1 - pos2
+        next_node = pos1 + difference
+
+        while not map.out_of_bounds(next_node):
+            resonant_antinodes.add(next_node)
+            next_node += difference
+
+        next_node = pos2 - difference
+        while not map.out_of_bounds(next_node):
+            resonant_antinodes.add(next_node)
+            next_node -= difference
+
+    return len(resonant_antinodes)
+
+
+def extract_antenna_map(file_name: str) -> Map:
+    input_text = read_input(file_name)
+    return Map(input_text)
+
+
+def part1(file_name: str) -> int:
+    map = extract_antenna_map(file_name)
+    return calculate_unique_antinodes(map)
+
+
+def part2(file_name: str) -> int:
+    map = extract_antenna_map(file_name)
+    return calculate_resonant_antinodes(map)
+
+
+if __name__ == '__main__':
+    result1 = part1('puzzle.txt')
+    print(f'The total number of unique antinodes is: {result1}')
+
+    result2 = part2('puzzle.txt')
+    print(f'The total number of unique resonant antinodes is: {result2}')
