802f20ad9b8cb949da0fff209dc484cd4c348946
Lukas Wilde <s8luwild@stud.uni-saarland.de>
2024-12-10 02:05:50 +0100

    fix directory resolution to work with pytest

--- utils/input.py
+++ utils/input.py
@@ -1,6 +1,7 @@
 import inspect
 from pathlib import Path
 from typing import List
+from re import search
 
 
 def read_input(file_name: str) -> str:
@@ -10,11 +11,14 @@ def read_input(file_name: str) -> str:
     with file.open('r') as f:
         return f.read()
 
+def get_project_root():
+    return Path(__file__).resolve().parent.parent
 
 def _correct_calling_directory(file_name: str) -> Path:
     # Extract the location of the calling function, to correctly get the absolute puzzle input path
-    calling_module = inspect.stack()[-1].filename
-    return Path(calling_module).resolve().parent / file_name
+    for frame in inspect.stack():
+        if day := search(r"day\d\d", frame.filename):
+            return get_project_root() / day[0] / file_name
 
 
 def read_split_input(file_name: str) -> List[str]:
